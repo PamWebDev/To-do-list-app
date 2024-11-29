72c6c97b2423994b1688a5b3adc792e4d1799bfe
Pamela Briceño Lopez <pampambriceno.dev@gmail.com>
2024-11-28 22:03:00 -0500

    feat: funcion para crear tareas

--- backend/routes/tasks_routes.py
+++ backend/routes/tasks_routes.py
@@ -2,9 +2,10 @@
 Este modulo contiene la ruta a /tasks y sus configuraciones
 """
 from flask import Blueprint
-from flask import jsonify
+from flask import jsonify, request
 from database.db import db
 from models.tasks_model import Tasks
+from datetime import datetime, timezone
 
 tasks_routes = Blueprint('tasks_routes', __name__)
 
@@ -14,3 +15,21 @@ def get_tasks():
     Obtiene la lista completa de las tareas
     """
     return jsonify([task.to_dict() for task in Tasks.query.all()])
+
+@tasks_routes.route('/api/tasks', methods=['POST'])
+def create_task():
+    """
+    Crea una tarea nueva
+    """
+    data = request.get_json()
+    task = Tasks(
+        title = data['title'],
+        description=data['description'],
+        status=data['status'],
+        created_at=data.get('createdAt', datetime.now(timezone.utc)),
+        updated_at=data.get('updatedAt', datetime.now(timezone.utc))
+    )
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.to_dict()), 201
+
