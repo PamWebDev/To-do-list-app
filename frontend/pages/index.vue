005ad50cbf6499d9c5542a39d60c416639670cac
Pamela Briceño Lopez <pampambriceno.dev@gmail.com>
2024-11-28 22:37:26 -0500

    feat: funciones para solicitudes GET, POST, PUT y DELETE

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,39 @@
 <template>
     <div>
-        <h1>Hello, world!</h1>
+      <h1>Lista de Tareas</h1>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">
+          <p>{{ task.title }}</p>
+          <p>{{ task.description }}</p>
+          <p>{{ task.status }}</p>
+          <p>{{ task.createdAt }}</p>
+          <button @click="deleteTask(task.id)">Eliminar</button>
+          <button @click="editTask(task.id)">Editar</button>
+        </li>
+      </ul>
     </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const tasks = ref([])
+
+const fetchTasks = async() => {
+    const res = await fetch('/api/tasks')
+    tasks.value = await res.json()
+}
+
+const deleteTask = async(id) => {
+    await fetch(`/api/tasks/${id}`, { method: 'DELETE'})
+    fetchTasks()
+}
+
+const editTask = (id) => {
+
+}
+
+onMounted(() => {
+    fetchTasks()
+})
 </script>
